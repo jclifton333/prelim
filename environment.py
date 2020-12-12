ff47c81f586fd5ccbc6a7903a4d76f4675a85d32
Jesse Clifton <jesseclifton@Jesses-MacBook-Pro.local>
2020-12-11 23:47:32 -0600

    (wip) disease counts diverging

--- environment.py
+++ environment.py
@@ -17,8 +17,8 @@ class PoissonDisease(object):
     Assuming seasonal variation only in endemic component.
     """
 
-    def __init__(self, L, distance_matrix, lambda_a=0.2, phi_a=0.1, alpha_nu=-3.12, alpha_lambda=-0.67, alpha_phi=-1.01,
-                 beta_nu=np.array([1.91, 2.69]), kernel_bandwidth=1):
+    def __init__(self, L, lambda_a=0.2, phi_a=0.1, alpha_nu=-3.12, alpha_lambda=-0.67, alpha_phi=-1.01,
+                 beta_nu=np.array([1.91, 2.69]), kernel_bandwidth=0.1):
         self.L = L
         self.alpha_nu = alpha_nu
         self.alpha_lambda = alpha_lambda
@@ -34,11 +34,17 @@ class PoissonDisease(object):
         # phi (spatiotemporal effect)
         self.phi = np.exp(self.alpha_phi)
 
+        # Generate coordinates uniformly on root_L x root_L square
+        root_L = np.sqrt(L)
+        coordinates = np.random.uniform(low=0, high=root_L, size=L)
+
         # Construct spatial weight matrix
         self.spatial_weight_matrix = np.zeros((L, L))
         for i in range(L):
             for j in range(i, L):
-                d_ij = distance_matrix[i, j]
+                coord_i = coordinates[i]
+                coord_j = coordinates[j]
+                d_ij = np.abs(coord_i - coord_j).sum()
                 weight_ij = np.exp(-d_ij / kernel_bandwidth)
                 self.spatial_weight_matrix[i, j] = weight_ij
                 self.spatial_weight_matrix[j, i] = weight_ij
@@ -52,10 +58,12 @@ class PoissonDisease(object):
         z = np.array([np.sin(self.omega * t), np.cos(self.omega * t)])
         log_nu = self.alpha_nu + np.dot(self.beta_nu, z)
         nu = np.exp(log_nu)
-
+        z = [z for _ in range(self.L)]
         return nu, z
 
     def reset(self):
+        # ToDo: option to reset coordinates?
+
         self.Y = np.random.poisson(1, size=self.L)
         self.t = 1
         self.X_list = []
@@ -72,15 +80,18 @@ class PoissonDisease(object):
         spatiotemporal_action = self.phi_a * spatial_weight_times_interaction
         mean_counts_ = endemic + autoregressive - autoregressive_action + spatiotemporal - spatiotemporal_action
         mean_counts_ = np.maximum(mean_counts_, 0)
-        return mean_counts_, spatial_weight_times_ytm1, spatial_weight_times_interaction
+        return mean_counts_, action_infection_interaction, spatial_weight_times_ytm1, spatial_weight_times_interaction
 
     def step(self, A):
+
         nu, z = self.get_endemic_effect(self.t)
-        mean_counts_, spatial_weight_times_ytm1, spatial_weight_times_interaction = self.mean_counts(self.Y, A, nu)
+        mean_counts_, action_infection_interaction, spatial_weight_times_ytm1, spatial_weight_times_interaction = \
+            self.mean_counts(self.Y, A, nu)
+        X_t = np.column_stack((np.ones(self.L), z, self.Y, action_infection_interaction, spatial_weight_times_ytm1,
+                               spatial_weight_times_interaction))
         Y = np.random.poisson(mean_counts_)
         self.Y = Y
         self.t += 1
-        X_t = np.column_stack((z, spatial_weight_times_ytm1, spatial_weight_times_interaction))
         self.X_list.append(X_t)
         self.Y_list.append(Y)
         return Y
--- model_estimation.py
+++ model_estimation.py
@@ -1,9 +1,41 @@
+import pdb
 import numpy as np
-import numba as nb
+from scipy.optimize import minimize
+from functools import partial
+from environment import PoissonDisease
 
 
 def negative_log_likelihood(param_vec, Y_stacked, X_stacked):
-    mean_counts_ = np.dot(X_stacked, param_vec)
+    param_vec[3] = np.exp(param_vec[3])
+    param_vec[5] = np.exp(param_vec[5])
+    endemic_term = np.exp(np.dot(X_stacked[:, :2], param_vec[:2]))
+    autoregressive_term = np.dot(X_stacked[:, 2:4], param_vec[2:4])
+    spatiotemporal_term = np.dot(X_stacked[:, 4:], param_vec[4:])
+
+    mean_counts_ = endemic_term + autoregressive_term + spatiotemporal_term
+    mean_counts_ = np.maximum(mean_counts_, 0.01)
     log_counts = np.log(mean_counts_)
     log_lik = np.dot(Y_stacked, log_counts) - np.sum(mean_counts_)
     return -log_lik
+
+
+def fit_model(env):
+    initial_param = np.concatenate(([env.alpha_nu], env.beta_nu, [env.alpha_lambda], [np.log(env.lambda_a)],
+                                    [env.alpha_phi], [np.log(env.phi_a)]))
+    Y_stacked = np.hstack(env.Y_list)
+    X_stacked = np.vstack(env.X_list)
+    nll_partial = partial(negative_log_likelihood, Y_stacked=Y_stacked, X_stacked=X_stacked)
+    result = minimize(nll_partial, x0=initial_param)
+    pdb.set_trace()
+    return result.x
+
+
+if __name__ == "__main__":
+    L = 50
+    T = 20
+    env = PoissonDisease(L)
+    env.reset()
+    for _ in range(T):
+        env.step(np.random.binomial(1, 0.1, L))
+    param_hat = fit_model(env)
+
