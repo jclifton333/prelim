df65f5d56b45aaad7824c9ada0a606981a8b1186
Jesse Clifton <jesseclifton@Jesses-MacBook-Pro.local>
2020-12-28 22:55:00 -0600

    add bandwidth filter

--- results/analyze_results.py
+++ results/analyze_results.py
@@ -8,11 +8,14 @@ import argparse
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--date', type=str)
+    parser.add_argument('--bandwidth_filter', type=str)
     args = parser.parse_args()
 
     date = args.date
+    bandwidth_filter = [float(b) for b in args.bandwidth_filter.split(',')]
 
-    summary_dict = {'policy': [], 'L': [], 'score': [], 'se': [], 'specified_kernel': []}
+    summary_dict = {'policy': [], 'L': [], 'score': [], 'se': [], 'specified_kernel': [],
+                    'bandwidth': []}
     for fname in os.listdir():
         if fname.endswith(".yml"):
             if date is None or date in fname:
@@ -22,11 +25,20 @@ if __name__ == "__main__":
                 score = d['score']
                 specified_kernel = d['specified_kernel']
                 se = d['se']
-                summary_dict['specified_kernel'].append(specified_kernel)
-                summary_dict['policy'].append(policy)
-                summary_dict['L'].append(L)
-                summary_dict['score'].append(score)
-                summary_dict['se'].append(se)
+                if 'global_kernel_bandwidth' in d.keys():
+                    bandwidth = d['global_kernel_bandwidth']
+                else:
+                    bandwidth = None
+
+                # Check filters
+                keep = bandwidth in bandwidth_filter
+                if keep:
+                    summary_dict['specified_kernel'].append(specified_kernel)
+                    summary_dict['bandwidth'].append(bandwidth)
+                    summary_dict['policy'].append(policy)
+                    summary_dict['L'].append(L)
+                    summary_dict['score'].append(score)
+                    summary_dict['se'].append(se)
     summary_df = pd.DataFrame.from_dict(summary_dict)
     summary_df.sort_values(by=['L', 'policy', 'specified_kernel'], inplace=True)
     print(summary_df)
