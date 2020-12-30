f48708aa2ba46650bb6bff6d7434dd3cb013947e
Jesse Clifton <jesseclifton@Jesses-MacBook-Pro.local>
2020-12-29 22:08:49 -0600

    change settings

--- run.sh
+++ run.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
-python3 run.py --policy_name='myopic_model_free' --L=15 --time_horizon=25 --budget=10 --num_replicates=1 \
-               --true_kernel='network' --specified_kernel='global' --global_kernel_bandwidth=10 \
-               --replicate_batches=1
+python3 run.py --policy_name='greedy_model_based' --L=50 --time_horizon=25 --budget=10 --num_replicates=10 \
+               --true_kernel='network' --specified_kernel='global' --global_kernel_bandwidth=20 \
+               --replicate_batches=5
+
+python3 run.py --policy_name='greedy_model_based' --L=50 --time_horizon=25 --budget=10 --num_replicates=10 \
+               --true_kernel='network' --specified_kernel='network' --global_kernel_bandwidth=20 \
+               --replicate_batches=5
